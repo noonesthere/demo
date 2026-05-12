abcbc237b84a729c68a681ab2af10cdcf29d8c9b
noonesthere <d.zlobenets@gmail.com>
2026-05-12 12:35:08 +0300

    add i18n api and other useful extensions

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,15 @@
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
+import * as path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
     resolve: {
         alias: {
-            src: '/src'
+            // src: '/src',
+             src: path.resolve(__dirname, './src'),
+             i18n: path.resolve(__dirname, process.env.NODE_ENV == 'production' ? 'build/i18n' : 'i18n')
         }
     },
     plugins: [
